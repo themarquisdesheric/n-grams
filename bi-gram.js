6c2d951b07d4d01fc3270cc5c4ebbde3c8c15ab4
Yuval Allweil <themarquisdesheric@gmail.com>
2025-10-19 18:49:08 -0700

    dictionary of bi-grams working

--- bi-gram.js
+++ bi-gram.js
@@ -9,10 +9,43 @@ const rl = readline.createInterface({
     crlfDelay: Infinity
 });
 
-async function readLines(filePath) {
+// moving window to hold each bi-gram (two-word seqeuence)
+const biGram = [];
+// map to hold each word and the words that follow it
+const nextWords = {};
+
+async function createBiGram(filePath) {
   for await (const line of rl) {
-    console.log(line);
+    const words = line.split(' ');
+
+    words.forEach((word) => {
+      // strip punctuation and convert to lowercase
+      const cleanWord = word.toLowerCase().replace(/[.;,\-_—:!?()"'`‘’“”]/g, '');
+      // skip empty punctuation-only "words"
+      if (cleanWord.length === 0) return;
+
+      biGram.push(cleanWord);
+
+      if (biGram.length === 2) {
+        if (!nextWords[biGram[0]]) {
+          nextWords[biGram[0]] = [biGram[1]];
+        } else {
+          nextWords[biGram[0]].push(biGram[1]);
+        }
+        // remove first word to maintain bi-gram window size
+        biGram.shift();
+      }
+    });
   }
+
+  return nextWords;
 }
 
-readLines(filePath).catch(console.error);
+(async () => {
+  try {
+    const biGrams = await createBiGram(filePath);
+    console.log(biGrams);
+  } catch (err) {
+    console.error(err);
+  }
+})();
